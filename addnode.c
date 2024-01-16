241669c81912e470ac9ab9952a95e349d2f8511d
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-01-16 01:04:22 -0800

    4. add

---
+++ addnode.c
@@ -0,0 +1,26 @@
+92% of storage used … Without storage, you can't save to Drive, back up to Photos, and use Gmail. Get 100 GB of storage for Ksh 250.00 Ksh 60.00/month for 3 months.
+addnode.c
+#include "monty.h"
+/**
+ * addnode - add node to the head stack
+ * @head: head of the stack
+ * @n: new_value
+ * Return: no return
+*/
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (aux)
+		aux->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
